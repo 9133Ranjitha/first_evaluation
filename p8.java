91e5dd5ac95eee574a731467c0a7ef30debb8c09
S.Ranjitha <112804191+9133Ranjitha@users.noreply.github.com>
2023-03-22 12:45:24 +0530

    Add files via upload
    Given a paragraph, count the number of words and sentences

---
+++ p8.java
@@ -0,0 +1,13 @@
+public class main{
+   public static void main(String[] args){
+     String input="ranju";
+     String bag="";
+     int count=1;
+      for(int i=0;i<input.length ()-1; i++ ){
+      bag+=input.charAt(i);
+      count++;
+      }
+      System.out.println(count);
+      }
+      }
+       
