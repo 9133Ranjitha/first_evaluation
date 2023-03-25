6d237aa72a9ba6afc6ad1785f8171306412cd042
S.Ranjitha <112804191+9133Ranjitha@users.noreply.github.com>
2023-03-25 21:44:51 +0530

    day3
    area of circle

---
+++ area.java
@@ -0,0 +1,18 @@
+class circle
+{
+	public static void main (String[] args) 
+	{
+		int radius=5;
+	  	double area=circle(radius);
+		//int radius=5;
+	        System.out.println(area);
+	}
+	public static double circle(double code)
+	{
+		return (22*code*code)/7;
+	}
+	
+}
+ 
+
+//formula:- area=(22*radius*radius)/7 ;
